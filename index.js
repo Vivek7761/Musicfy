8c01a6d35e39f7568a1f7b0b4af80fa14a94dde3
Vivek7761 <mahtov4@gmail.com>
2022-09-18 21:13:51 +0530

    music

---
+++ index.js
@@ -0,0 +1,76 @@
+// console.log('Hello world');
+// var myName =  'Vivek';
+// console.log(myName);
+// console.log(typeof(myName));
+// var myPhoneNumber = 8756657656;
+// var myHome = "Bhandaridah";
+// console.log(myPhoneNumber);
+// console.log(myHome);
+// console.log(3*3);
+// console.log("remainder"  +21%5);
+//if else
+
+// var tomr = "sunny";
+// if(tomr == "rain"){
+//     console.log('take the rain coat');
+// }else{
+//     console.log("no need");
+// }
+
+//leap yrar
+
+// var year = 2020;
+// debugger;
+// if(year % 4 === 0){
+//     if( year % 100 ===0){
+//     if( year % 400 ===0){
+//         console.log("This year" + year + "is leap year");
+//     }else{
+//         console.log("This year" + year + "is  not leap year");
+//     }
+//     }else{
+//         console.log("This year" + year + "is  not leap year");
+//     }
+//     }else{
+//         console.log("This year" + year + "is  not leap year");
+//     }
+
+// TABLE
+//  for(var num = 1; num<=10; num++){
+//     var tableOf = 8;
+//     console.log(tableOf + " * " + num + " = " + tableOf * num);
+//  }
+// var myName = " vivek kumar";
+// console.log(myName);
+
+// myName = " stock market";
+// console.log(myName);
+
+// let myName = " vivek kumar";
+// console.log(myName);
+
+// myName = " stock market";
+// console.log(myName);
+
+// const myName = " vivek kumar";
+// console.log(myName);
+
+// myName = " stock market";
+// console.log(myName);
+
+
+
+
+biodata();
+function biodata(){
+    var myFirstname = "vivek";
+    console.log(myFirstname);
+
+    
+    if(true){
+        var myLastname = "Mahto";
+        console.log('inner' + myLastname);
+        console.log('inner' + myFirstname);
+    }
+    console.log('innerOuter' + myLastname)
+}
